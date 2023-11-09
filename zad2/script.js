69172845cf63001d12cb80b953a3d24491b19c86
Daniel Chojnicki <djchojnicki@gmail.com>
2023-11-09 09:46:07 +0100

    zad3

--- zad2/script.js
+++ zad2/script.js
@@ -2,10 +2,12 @@ const number = document.getElementById('number');
 const incrementBtn = document.getElementById('increment');
 const changeColorBtn = document.getElementById('change-color');
 
-incrementBtn.addEventListener('click', () =>{
-    number.textContent = parseInt(number.textContent)+1;
-})
-
-changeColorBtn.addEventListener('click', () => {
-    number.classList.toggle('color-red');
-})
+onload = () => {
+    incrementBtn.addEventListener('click', () =>{
+        number.textContent = parseInt(number.textContent)+1;
+    });
+    
+    changeColorBtn.addEventListener('click', () => {
+        number.classList.toggle('color-red');
+    });
+}
