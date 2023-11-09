6c754494623527a1ababd88cb7257727aced2c12
Daniel Chojnicki <djchojnicki@gmail.com>
2023-11-09 09:31:12 +0100

    zad2

---
+++ zad2/script.js
@@ -0,0 +1,11 @@
+const number = document.getElementById('number');
+const incrementBtn = document.getElementById('increment');
+const changeColorBtn = document.getElementById('change-color');
+
+incrementBtn.addEventListener('click', () =>{
+    number.textContent = parseInt(number.textContent)+1;
+})
+
+changeColorBtn.addEventListener('click', () => {
+    number.classList.toggle('color-red');
+})
