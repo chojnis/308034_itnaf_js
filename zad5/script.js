04e7627419c7b16d9754724d3891084798e4a001
Daniel Chojnicki <djchojnicki@gmail.com>
2023-11-09 10:53:13 +0100

    added less operational fibonacci2

--- zad5/script.js
+++ zad5/script.js
@@ -1,10 +1,11 @@
 onload = () => {
     console.log(fibonnaci(9));
+    console.log(fibonnaci2(9));
 }
 
 const fibonnaci = (n) => {
     let result = [];
-    for(let i=0; i<9; i++){
+    for(let i=0; i<n; i++){
         result.push(computeFibonnaci(i));
     }
 
@@ -14,4 +15,15 @@ const fibonnaci = (n) => {
 const computeFibonnaci = (n) => {
     if(n<2) return n;
     return computeFibonnaci(n-1) + computeFibonnaci(n-2);
+}
+
+// less operations
+var fibonacciR = [];
+const fibonnaci2 = (n) => {
+    if(fibonacciR.at(n) === undefined){
+        if(n<2) fibonacciR[n] = n;
+        else fibonacciR[n] = fibonnaci2(n-1)[n-1] + fibonnaci2(n-2)[n-2];
+    }
+
+    return fibonacciR;
 }
